f0508669b6565cec738dd00e5d874f855e9de45e
U-DESKTOP-NEBUJ7A\johnp <you@example.com>
2016-07-04 22:38:34 -0400

    uh

---
+++ git_comm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in `seq 10` 
+do
+	#python script goes here
+	#should just be able to write a shell command here
+	#interstingly in C it would be the executable but in python 
+	#we can just use the python executable
+	#one add on is to have it commit the file itself
+	quone.py
+	git add quone.py
+	git commit -m "commit #x"
+	git push origin master
+done
+

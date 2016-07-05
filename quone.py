300fcfca5a874b45be8b8b19775f84667eb75574
johntiger1 <john.person1@gmail.com>
2016-07-05 00:03:08 -0400

    commit #x

--- quone.py
+++ quone.py
@@ -67,3 +67,9 @@ file.close()
 # not code injection
 # not code injection
 # not code injection
+# not code injection
+# not code injection
+# not code injection
+# not code injection
+# not code injection
+# not code injection

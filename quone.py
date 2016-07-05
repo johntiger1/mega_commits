3fd2296465f5e83ae542b48aafb8cde39e281e7c
johntiger1 <john.person1@gmail.com>
2016-07-04 22:32:10 -0400

    the first part

---
+++ quone.py
@@ -0,0 +1,17 @@
+file = open("quone.py", "ab")
+# print file
+
+# print (file.read())
+
+file.write("\n# not code injection")
+
+file.close()
+
+### THE DEADLANDS ###
+
+# not code injection
+# not code injection
+# not code injection
+
+# not code injection
+# not code injection

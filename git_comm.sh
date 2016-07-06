c144b34a046c790bb62b17ea226c6c4e74c7dd91
johntiger1 <john.person1@gmail.com>
2016-07-05 23:41:10 -0400

    testing var sub john

--- git_comm.sh
+++ git_comm.sh
@@ -7,6 +7,7 @@ do
 	#interstingly in C it would be the executable but in python 
 	#we can just use the python executable
 	#one add on is to have it commit the file itself
+	#actually this is not even an easy thing to do
 	python ./quone.py 
 	git add quone.py &> /dev/null
 	git commit -m "commit #x" &> /dev/null 
